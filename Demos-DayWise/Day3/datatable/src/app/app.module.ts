c4a587a45121f05de04b58d7d218b243c5627086
sonam-niit <sonamlovesmumypapa@gmail.com>
2023-03-23 16:35:36 +0530

    Backedn Code

--- Demos-DayWise/Day3/datatable/src/app/app.module.ts
+++ Demos-DayWise/Day3/datatable/src/app/app.module.ts
@@ -8,12 +8,18 @@ import { UserlistComponent } from './components/userlist/userlist.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { DynamicComponent } from './components/dynamic/dynamic.component';
+import { SigninComponent } from './jwt/signin/signin.component';
+import { SignupComponent } from './jwt/signup/signup.component';
+import { UserProfileComponent } from './jwt/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserlistComponent,
-    DynamicComponent
+    DynamicComponent,
+    SigninComponent,
+    SignupComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
